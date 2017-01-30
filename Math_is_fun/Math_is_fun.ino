c6e8db79427cf0b7664672aefb01922f8e784b09
Cristofer <cristofer.ok23@gmail.com>
2017-01-30 21:15:59 +0100

    Math_is_fun

--- Math_is_fun/Math_is_fun.ino
+++ Math_is_fun/Math_is_fun.ino
@@ -20,26 +20,26 @@ void setup()                // run once, when the sketch starts
 
   Serial.println("Here's some math: ");
 
-  Serial.print("a = 5");
- 
+  Serial.print("a = ");
+  Serial.println(a);
   Serial.print("b = ");
   Serial.println(b);
   Serial.print("c = ");
   Serial.println(c);
 
-  Serial.print("a + b = ");   // add
+  Serial.print("a + b = ");      // add
   Serial.println(a + b);
 
-  Serial.print("a * c = ");    // multiply
+  Serial.print("a * c = ");      // multiply
   Serial.println(a * c);
 
-  Serial.print("c / b = ");   // divide
+  Serial.print("c / b = ");      // divide
   Serial.println(c / b);
 
-  Serial.print("b - c =");    // subtract
+  Serial.print("b - c =");      // subtract
   Serial.println(b - c);
 
-  Serial.print("c % b =");    //module
+  Serial.print("c % b =");     // module
   Serial.println(c % b);
 }
 
