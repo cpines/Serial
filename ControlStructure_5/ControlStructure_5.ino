4ac9386c3ae12cded0656a1c12fc9320a2f80356
Cristofer <cristofer.ok23@gmail.com>
2017-02-27 20:14:37 +0100

    ControlStructure_4

--- ControlStructure_5/ControlStructure_5.ino
+++ ControlStructure_5/ControlStructure_5.ino
@@ -8,41 +8,30 @@
 //********** Includes *************************************************************
 
 //********** Variables ************************************************************
-float qualEnergy= 3;
+int sensorReading = 4;
 
 //********** Setup ****************************************************************
-void setup()              // run once, when the sketch starts
+void setup()
 {
-  Serial.begin(9600); 
-  
-  if (qualEnergy >= 43.2)
-  {
-    Serial.print("G");
-  }
-  else if (qualEnergy < 43.2 & qualEnergy > 38.2)
-  {
-    Serial.print("F");
-  }
-  else if (qualEnergy <= 38.2 & qualEnergy > 17.7)
-  {
-    Serial.print("E");
-  }
-  else if (qualEnergy <= 17.7 & qualEnergy > 11.1)
-  {
-    Serial.print("D");
-  }
-  else if (qualEnergy <= 11.1 & qualEnergy > 6.5)
-  {
-    Serial.print("C");
-  }
-  else if (qualEnergy <= 6.5 & qualEnergy > 3.5)
-  {
-    Serial.print("B");
-  }
-  else
-  {
-    Serial.print("A");
-  }
+  Serial.begin(9600);
+  Serial.print("The day is "); 
+   
+  switch (sensorReading) {
+  case 0:    
+    Serial.println("dark");
+    break;
+  case 1:    
+    Serial.println("dim");
+    break;
+  case 2:    
+    Serial.println("medium");
+    break;
+  case 3:
+    Serial.println("bright");
+    break;
+  default:
+    Serial.println("... I don't know!!!");
+  } 
 }
 
 //********** Loop *****************************************************************
